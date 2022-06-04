ffc6881602ab9d5f3f2d5cc23a4631d1b205a98a
ChandanPk <chandanpk9@gmail.com>
2022-06-04 03:08:05 -0700

    Create music page

--- src/comps/Music.js
+++ src/comps/Music.js
@@ -1,13 +1,18 @@
 import styles from '../styles/MusicPage.module.css'
-import musicImg from '../assests/tabtxt.png'
-
+import musicImg from '../assests/croped.png'
+import Navbar from './Navbar'
+import mobileViewImg from '../assests/musicMobileView.png'
 const Music = () => {
     return (
         <>
+            <Navbar />
             <div className={styles.container}>
-                <div className={styles.musicImgContainer}>
-                    <img className={styles.imgTag} src={musicImg} alt="test" />
-                </div>
+                    <div className={styles.musicImgContainer}>
+                        <img className={styles.imgTag} src={musicImg} alt="test" />
+                    </div>
+                    <div className={styles.musicImgContainer2}>
+                        <img className={styles.imgTag} src={mobileViewImg} alt="test" />
+                    </div>
             </div>
         </>
     );
